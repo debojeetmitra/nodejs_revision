5d5770d3b54433f103de308b506426f4d7dfc75f
Debojeet Mitra <debojeetmitra04@gmail.com>
2026-07-03 15:33:26 +0530

    Done with express in nodejs !!

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require('fs');
 const url = require('url');
 
-const server = http.createServer((req, res) => {
+function myHandler(req, res){
   if (req.url === "/favicon.ico") return res.end();
   const log = `${Date.now()}: ${req.url} New Req Received \n` 
   const myUrl = url.parse(req.url, true);
@@ -23,7 +23,10 @@ const server = http.createServer((req, res) => {
         default: res.end("404")
     }
   })
-}); //Creates a http server for a user
+}
+
+
+const server = http.createServer(myHandler); //Creates a http server for a user
 
 server.listen(8000, () => console.log("Server started"));
 
