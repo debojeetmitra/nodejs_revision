9e09aef08b59349d764c5d7b86a225e6acbe985b
Debojeet Mitra <debojeetmitra04@gmail.com>
2026-08-24 16:03:31 +0530

    feat: implement PATCH and DELETE methods for user routes and update task status documentation

--- project-1/index.js
+++ project-1/index.js
@@ -19,11 +19,28 @@ app.get('/api/users', (req, res) => {
     return res.json(users)
 })
 
-app.get('/api/users/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
-    return res.json(user);
-});
+app
+    .route('/api/users/:id')
+    .get((req, res) => {
+        const id = Number(req.params.id);
+        const user = users.find((user) => user.id === id);
+        return res.json(user);
+    })
+    .patch((req, res) => {
+        //Edit user with id
+        return res.json({ status: "Pending" });
+    })
+    .delete((req, res) => {
+        //Delete user with id
+        return res.json({ status: Pending })
+    })
+
+
+app.post('/api/users', (req, res) => {
+    //Todo status pending
+    return res.json({ status: "pending" })
+})
+
 
 
 
