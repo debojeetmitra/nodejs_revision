06f1f1a1d0ff5d9c22497fe21dd17c9727533565
Debojeet Mitra <debojeetmitra04@gmail.com>
2026-09-01 12:15:04 +0530

    Learnt about HTTP status codes and implemented it and tested via Postman

--- project-1/index.js
+++ project-1/index.js
@@ -5,13 +5,13 @@ const fs = require('fs')
 const app = express();
 const PORT = 8000;
 
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use((req, res, next)=> {
+app.use((req, res, next) => {
     console.log('Hello from middleware 1');
     req.myUserName = 'debo.dev';
     next();
-    
+
 })
 
 app.use((req, res, next) => {
@@ -41,6 +41,7 @@ app
     .get((req, res) => {
         const id = Number(req.params.id);
         const user = users.find((user) => user.id === id);
+        if (!user) return res.status(404).json({ error: "user not found bro !!"})
         return res.json(user);
     })
     .patch((req, res) => {
@@ -53,25 +54,30 @@ app
     })
 
 
-    app.post('/api/users', (req, res) => {
+app.post('/api/users', (req, res) => {
 
-        // Todo status pending
-        const body = req.body;
-    
-        users.push({ ...body, id: users.length + 1 });
-    
-        fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    
-            if (err) {
-                return res.status(500).json({ status: "error" });
-            }
-    
-            return res.json({
+    // Todo status pending
+    const body = req.body;
+
+    if(!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title){
+        return res.status(400).json({msg: "All fields req bro !!!"})
+    }
+
+    users.push({ ...body, id: users.length + 1 });
+
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+
+        if (err) {
+            return res.status(500).json({ status: "error" });
+        }
+
+        return res.status(201)
+            .json({
                 status: "success",
                 values: users
             });
-        });
     });
+});
 
 
 
