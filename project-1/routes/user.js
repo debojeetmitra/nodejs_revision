f4d26e6279fbbb9e18a4b485f0b87a2856e42347
Debojeet Mitra <debojeetmitra04@gmail.com>
2026-09-11 22:08:43 +0530

    Refactor user routes and controller for improved CRUD operations
    - Consolidated user route handlers into a dedicated controller.
    - Implemented GET, POST, PATCH, and DELETE methods for user management.
    - Enhanced error handling and response messages for better API usability.

--- project-1/routes/user.js
+++ project-1/routes/user.js
@@ -1,47 +1,19 @@
 const express = require('express')
+const { handleGetAllUsers, handleGetUserById,
+    handleUpdateUserById, handleDeleteUserById
+    , handleCreateNewUser
+} = require("../controllers/user")
+
 const router = express.Router();
 
 
-router.get('/',async  (req, res) => {
-    const allDbUsers = await User.find();
-    return res.json(allDbUsers)
-})
+router.route('/').get(handleGetAllUsers).post(handleCreateNewUser);
 
 router
     .route('/:id')
-    .get(async(req, res) => {
-        const user = await User.findById(req.params.id);
-        if (!user) return res.status(404).json({ error: "user not found bro !!"})
-        return res.json(user);
-    })
-    .patch(async(req, res) => {
-        await User.findByIdAndUpdate(req.params.id, { lastName: "Changed"});
-        return res.json({ status: "Success" });
-    })
-    .delete(async(req, res) => {
-        await User.findByIdAndDelete(req.params.id)
-        return res.json({ status: Success })
-    })
+    .get(handleGetUserById)
+    .patch(handleUpdateUserById)
+    .delete(handleDeleteUserById)
 
-
-router.post('/',async (req, res) => {
-
-    // Todo status pending
-    const body = req.body;
-
-    if(!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title){
-        return res.status(400).json({msg: "All fields req bro !!!"})
-    }
-
-    const result = await User.create({
-        firstName: body.first_name,
-        lastName: body.last_name,
-        email: body.email,
-        gender: body.gender,
-        jobTitle: body.job_title
-    })   
-
-    return res.status(201).json({ msg: "Success"})
-});
 
 module.exports = router;
